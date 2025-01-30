a5c115548ed7f0a393b7bdd1b3d26c04822f856d
&D3xt3rr <mathieu.thibeault-thuot@outlook.com>
2025-01-30 17:19:42 -0500

    add some plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,6 +12,56 @@ return {
       require "configs.lspconfig"
     end,
   },
+
+  { "nvim-tree/nvim-web-devicons", opts = {} },
+  {
+    "sindrets/diffview.nvim"
+  },
+  
+  -- lua/plugins/rose-pine.lua
+  { 
+	  "rose-pine/neovim", 
+	  name = "rose-pine",
+	  config = function()
+		  vim.cmd("colorscheme rose-pine")
+	  end
+  }
+  
+
+  {
+    "isakbm/gitgraph.nvim",
+    opts = {
+      symbols = {
+        merge_commit = 'M',
+        commit = '*',
+      },
+      format = {
+        timestamp = '%H:%M:%S %d-%m-%Y',
+        fields = { 'hash', 'timestamp', 'author', 'branch_name', 'tag' },
+      },
+      hooks = {
+        on_select_commit = function(commit)
+          vim.notify('DiffviewOpen ' .. commit.hash .. '^!')
+          vim.cmd(':DiffviewOpen ' .. commit.hash .. '^!')
+        end,
+        -- Check diff from commit a -> commit b
+        on_select_range_commit = function(from, to)
+          vim.notify('DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+          vim.cmd(':DiffviewOpen ' .. from.hash .. '~1..' .. to.hash)
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>gl",
+        function()
+          require('gitgraph').draw({}, { all = true, max_count = 5000 })
+        end,
+        desc = "GitGraph - Draw",
+      },
+    },
+  },
+
   {
     "williamboman/mason.nvim",
     opts = {
